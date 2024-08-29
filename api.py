493a2ed175ce824f18fb505d30800475518c0082
dedeamor12 <andreleandromatos@gmail.com>
2024-08-29 12:28:36 -0300

    vmconfigurada

--- api.py
+++ api.py
@@ -1,11 +1,24 @@
-from flask import Flask , jsonify, request
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///livros.db'
+
+db = SQLAlchemy(app)
+
+class Livro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 livros = [
     {'id': 1, 'titulo': '1984', 'autor': 'George Orwell'},
-    {'id': 2, 'titulo': 'O Senhor dos Anéis', 'autor': 'J. R. R.Tolkien'},
-    {'id': 3, 'titulo': 'O Prisioneiro de Azkaban', 'autor': 'J. K. Rowling'},
+    {'id': 2, 'titulo': 'O Senhor dos Anéis', 'autor': 'J. R. R. Tolkien'},
+    {'id': 3, 'titulo': 'O Prisioneiro de Azkaban', 'autor': 'J. K. Rowling'}
 ]
 
 @app.route('/api/livros', methods=['GET'])
